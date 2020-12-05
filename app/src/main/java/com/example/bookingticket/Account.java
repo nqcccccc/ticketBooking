7582735b4765187a8785cb225f26912b9d9fbe8e
Rosa <rosa121120@gmail.com>
2020-12-05 13:40:57 +0700

    Thêm màn hình nội dung & đặt vé

--- app/src/main/java/com/example/bookingticket/Account.java
+++ app/src/main/java/com/example/bookingticket/Account.java
@@ -1,4 +1,12 @@
 package com.example.bookingticket;
 
+import android.widget.TextView;
+
 public class Account {
+    private TextView
 }
+/*
+code login http://h0akd.blogspot.com/2012/04/huong-dan-lam-form-ang-nhap-voi-web.html
+code upload images http://www.codeplayon.com/2018/11/android-image-upload-to-server-from-camera-and-gallery/
+retrofit upload images https://stackoverflow.com/questions/62983294/upload-image-in-android-studio
+*/
